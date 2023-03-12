54dee616bfb874a7affd543bf57ab5ac6753b5bd
Tch1b0 <Jpour3006@gmail.com>
2023-03-12 17:00:06 +0100

    initial commit

---
+++ app/src/main/java/de/puncty/app/MainActivity.java
@@ -0,0 +1,15 @@
+package de.puncty.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.puncty.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
