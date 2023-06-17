dd31def139eacb36fa7f9dad0bd4cc2035c913c0
Tch1b0 <Jpour3006@gmail.com>
2023-06-17 12:49:01 +0200

    fix target api level

--- app/src/main/java/de/puncty/app/SplashScreenActivity.java
+++ app/src/main/java/de/puncty/app/SplashScreenActivity.java
@@ -3,6 +3,7 @@ package de.puncty.app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,14 +18,10 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         Handler delay = new Handler();
         SplashScreenActivity context = this;
-        Runnable run = new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(context, MainActivity.class);
-                startActivity(intent);
-            }
-        };
-        delay.postDelayed(run, 1000);
+        delay.postDelayed(() -> {
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
+        }, 1000);
     }
 
 
